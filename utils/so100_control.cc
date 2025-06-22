39679dc536bed7f95c388ea72ade709d7e8cf05c
hsmoon5458 <hsmoon5458@gmail.com>
2025-06-22 00:06:56 -0700

    separate controller handler

---
+++ utils/so100_control.cc
@@ -0,0 +1,79 @@
+#include "robot/actuation/factory/motor_factory.h"
+#include "robot/config/config_utils.h"
+#include "utils/so100_xbox_controller_handler.h"
+#include <glog/logging.h>
+#include <vector>
+#include <unistd.h> // For sleep
+#include <memory>   // For std::unique_ptr
+
+namespace {
+    constexpr int kSetupTime = 2;
+} // namespace
+
+int main(int argc, char* argv[]) {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = 1; // Log messages to stderr
+
+    try {
+        robot::Robot robot_config = robot::config_util::LoadRobotConfig("robot/config/robot_config.pbtxt");
+        auto number_of_motors = robot_config.actuations().single_actuation_size();
+        LOG(INFO) << "Robot Name: " << robot_config.name();
+        LOG(INFO) << "ID:" << robot_config.id();
+
+        // Motor instantiation.
+        robot::actuation::MotorFactory motor_factory;
+        std::vector<std::unique_ptr<robot::actuation::MotorInterface>> motors;
+
+        for (int i = 0; i < number_of_motors; i++) {
+            const auto& single_actuation = robot_config.actuations().single_actuation(i);
+            const auto& motor_proto = single_actuation.motor();
+
+            switch (motor_proto.motor_type()) {
+            case robot::actuation::MotorType::STS3215:
+                motors.emplace_back(motor_factory.CreateMotor(motor_proto));
+                break;
+            default:
+                LOG(ERROR) << "Unknown motor type: " << motor_proto.motor_type();
+                break;
+            }
+        }
+
+        // Initial motor setup.
+        for (int i = 0; i < number_of_motors; ++i) {
+            if (motors[i]) {
+                motors[i]->SetTorque(1);
+                motors[i]->SetMiddlePosition();
+            }
+        }
+        sleep(kSetupTime);
+
+        // Initialize and start So100XboxControllerHandler
+        utils::So100XboxControllerHandler controller_handler(robot_config, motors);
+        if (!controller_handler.Init()) {
+            LOG(ERROR) << "Failed to initialize So100XboxControllerHandler. Exiting.";
+            return 1;
+        }
+        controller_handler.Start();
+
+        for (int i = 0; i < number_of_motors; ++i) {
+            if (motors[i]) {
+                motors[i]->SetIdlePosition();
+            }
+        }
+        sleep(kSetupTime);
+
+        LOG(INFO) << "Disabling torque on all servos...";
+        for (auto& servo : motors) {
+            if (servo) {
+                servo->SetTorque(0);
+            }
+        }
+
+    } catch (const std::exception& e) {
+        LOG(ERROR) << "Error: " << e.what();
+        return 1;
+    }
+
+    LOG(INFO) << "Program terminated successfully.";
+    return 0;
+}
---
+++ utils/so100_xbox_controller_handler.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "utils/xbox_controller/xbox_controller.h"
+#include "robot/config/robot.pb.h"
+#include "robot/actuation/interfaces/motor_interface.h"
+#include <vector>
+#include <memory>
+#include <atomic>
+#include <thread>
+#include <map>
+
+namespace utils {
+
+class So100XboxControllerHandler {
+public:
+    So100XboxControllerHandler(const robot::Robot& robot_config, 
+                          std::vector<std::unique_ptr<robot::actuation::MotorInterface>>& motors);
+    ~So100XboxControllerHandler();
+
+    bool Init();
+    void Start();
+    void Stop();
+
+private:
+    void join();
+    void control_loop();
+    int MapRange(int value, int in_min, int in_max, int out_min, int out_max);
+
+    const robot::Robot& robot_config_;
+    std::vector<std::unique_ptr<robot::actuation::MotorInterface>>& motors_;
+    utils::XboxController xbox_controller_;
+    utils::XboxControllerState controller_state_;
+    std::thread xbox_event_thread_; // Reading the xbox status.
+    std::thread motor_control_thread_; // Main ControlLoop.
+    std::atomic<bool> stop_flag_{false};
+
+    static constexpr int kJoystickDeadZone = 5000;
+    static constexpr auto kPositionStep = 10;
+    
+    std::map<int, int> kXboxServoMap_ = {
+        {ABS_HAT0X, 0},
+        {ABS_Y, 1},
+        {ABS_RY, 2},
+        {BTN_WEST, 3}, // Corresponds to Y button for decreasing position
+        {BTN_SOUTH, 3},// Corresponds to A button for increasing position
+        {BTN_TL, 4},   // Left Bumper
+        {BTN_TR, 4},   // Right Bumper
+        {ABS_RZ, 5}    // Right Trigger
+    };
+};
+
+} // namespace utils
