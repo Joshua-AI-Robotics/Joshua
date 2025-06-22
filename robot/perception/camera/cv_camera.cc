3acd9bd946d303e5add80cac1482583f4fe6012f
hsmoon5458 <hsmoon5458@gmail.com>
2025-06-22 00:38:15 -0700

    update camera

--- robot/perception/camera/cv_camera.cc
+++ robot/perception/camera/cv_camera.cc
@@ -6,6 +6,16 @@ namespace perception {
 CvCamera::CvCamera() {
     // Open the default camera (device 0).
     cap_.open(0);
+
+    cap_.set(cv::CAP_PROP_FRAME_WIDTH, 1920);
+    cap_.set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
+    // cap_.set(cv::CAP_PROP_FPS, 5);
+
+    // LOG(INFO) <<  cap_.get(cv::CAP_PROP_FRAME_WIDTH);
+    // LOG(INFO) <<  cap_.get(cv::CAP_PROP_FRAME_HEIGHT);
+    // LOG(INFO) <<  cap_.get(cv::CAP_PROP_FPS);
+
+    
     if (!cap_.isOpened()) {
         // TODO: Replace with a proper logging mechanism.
         LOG(ERROR) << "ERROR: Could not open camera";
