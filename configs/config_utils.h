aaf5354181b090971b8c598cc332e0850937cec6
hsmoon5458 <hsmoon5458@gmail.com>
2025-07-09 18:48:52 -0700

    update configs

---
+++ configs/config_utils.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+
+#include "glog/logging.h"
+#include "google/protobuf/text_format.h"
+#include "configs/proto/config.pb.h"
+
+namespace configs::config_util{
+
+    configs::Config LoadConfig(const std::string& config_path) {
+    configs::Config config;
+    std::ifstream input(config_path);       
+    if (!input) {
+        LOG(ERROR) << "Failed to open config file: " << config_path;
+        throw std::runtime_error("Failed to open config file: " + config_path);
+    }
+    std::string config_content((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+    if (!google::protobuf::TextFormat::ParseFromString(config_content, &config)) {
+        LOG(ERROR) << "Failed to parse config from file: " << config_path;
+        throw std::runtime_error("Failed to parse config from file: " + config_path);
+    }
+    return config;
+    }
+}
