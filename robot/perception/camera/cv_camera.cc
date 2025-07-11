8a5e80263839985cee61567544c315afcc6735dc
hsmoon5458 <hsmoon5458@gmail.com>
2025-07-10 17:05:00 -0700

    camera config setup updated

--- robot/perception/camera/cv_camera.cc
+++ robot/perception/camera/cv_camera.cc
@@ -3,6 +3,8 @@
 #include <vector>
 #include <chrono>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <stdexcept>
 
 namespace robot::perception {
 
@@ -10,22 +12,29 @@ CvCamera::CvCamera(const robot::perception::Sensor& sensor_config) {
     auto camera_config = sensor_config.camera_config();
     camera_id_ = camera_config.id();
     id_ = GetId();
-    // Open the default camera (device 0).
-    cap_.open(0);
-
-    // cap_.set(cv::CAP_PROP_FRAME_WIDTH, 1920);
-    // cap_.set(cv::CAP_PROP_FRAME_HEIGHT, 1080);
-    // cap_.set(cv::CAP_PROP_FPS, 5);
-
-    // LOG(INFO) <<  cap_.get(cv::CAP_PROP_FRAME_WIDTH);
-    // LOG(INFO) <<  cap_.get(cv::CAP_PROP_FRAME_HEIGHT);
-    // LOG(INFO) <<  cap_.get(cv::CAP_PROP_FPS);
-
-    
+    cap_.open(camera_id_, cv::CAP_V4L2);
     if (!cap_.isOpened()) {
-        // TODO: Replace with a proper logging mechanism.
-        LOG(ERROR) << "ERROR: Could not open camera";
+        LOG(ERROR) << "ERROR: Could not open camera with id " << camera_id_;
+        throw std::runtime_error("Could not open camera with id " + std::to_string(camera_id_));
     }
+    bool set_fourcc = cap_.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    bool set_width = cap_.set(cv::CAP_PROP_FRAME_WIDTH, camera_config.width());
+    bool set_height = cap_.set(cv::CAP_PROP_FRAME_HEIGHT, camera_config.height());
+    bool set_fps = cap_.set(cv::CAP_PROP_FPS, camera_config.fps());
+
+    // Some drivers return false even on success.
+    if (!set_fourcc || !set_width || !set_height || !set_fps) {
+        LOG(ERROR) << "Setting camera resolution returned false. "
+                     << "set_fourcc=" << std::boolalpha << set_fourcc << ", "
+                     << "set_width=" << std::boolalpha << set_width << ", "
+                     << "set_height=" << std::boolalpha << set_height << ", "
+                     << "set_fps=" << std::boolalpha << set_fps;
+        throw std::runtime_error("Failed to set camera resolution.");
+    }
+
+    LOG(INFO) << "ID: " << id_;
+    LOG(INFO) << "Camera resolution: " << cap_.get(cv::CAP_PROP_FRAME_WIDTH) << "x" << cap_.get(cv::CAP_PROP_FRAME_HEIGHT);
+    LOG(INFO) << "Camera FPS: " << cap_.get(cv::CAP_PROP_FPS);
 }
 
 CvCamera::~CvCamera() {
