f9697ee46bced5d770bbb6c9d4d096412de5e039
hmoon <hsmoon5458@gmail.com>
2025-07-25 11:48:58 -0700

    mock input updated

--- ros2/actuation_subscriber.cc
+++ ros2/actuation_subscriber.cc
@@ -1,21 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
 #include "config/proto/robot.pb.h"
 #include "config/config_utils.h"
 #include "robot/actuation/factory/actuation_factory.h"
-#include <gflags/gflags.h>
 #include <thread>
 
 class ActuationSubscriber : public rclcpp::Node {
+
 public:
   ActuationSubscriber(const std::string& actuation_topic) : Node("actuation_subscriber") {
-
-    // TODO: parse gflags to select actuation topic.
+    subscription_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+      "actuation_input", 10, std::bind(&ActuationSubscriber::actuation_callback, this, std::placeholders::_1));
     
-    // Default is keyboard input.
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "keyboard_input", 10, std::bind(&ActuationSubscriber::keyboard_callback, this, std::placeholders::_1));
-
     config::Config config = config::config_util::LoadConfig("config/config_preset/so100_with_example_ai.pbtxt");
     
     robot::actuation::ActuationFactory actuation_factory;
@@ -23,6 +19,12 @@ public:
     for (const auto& single_actuation : config.robot().actuations().single_actuation()) {
       const auto& actuator_proto = single_actuation.actuator();
       actuators_.push_back(actuation_factory.CreateActuator(actuator_proto));
+    }
+
+    for (const auto& single_actuation : config.robot().actuations().single_actuation()) {
+      const auto& actuator_proto = single_actuation.actuator();
+      // TODO: Move operational limits to actuator proto, not motor specific proto.
+      actuation_limits_.push_back(std::make_pair(actuator_proto.sts3215_config().operational_lower_limit(), actuator_proto.sts3215_config().operational_upper_limit()));
     }
 
     if (actuators_.empty()) {
@@ -55,23 +57,24 @@ public:
   }
 
 private:
-  void keyboard_callback(const std_msgs::msg::String::SharedPtr keyboard_msg) {
-    RCLCPP_INFO(this->get_logger(), "Received keyboard input: %s", keyboard_msg->data.c_str());
-    
-    // TODO: Implement actuation logic based on keyboard input
-    // For now, just log the received key
+  void actuation_callback(const std_msgs::msg::Float32MultiArray::SharedPtr actuation_msg) {
+    for (size_t i = 0; i < actuation_msg->data.size() && i < actuators_.size(); ++i) {
+      float actuation_value = actuation_msg->data[i];
+      RCLCPP_INFO(this->get_logger(), "Actuator %zu: %f", i, actuation_value);
+      
+      auto middle_value = (actuation_limits_[i].first + actuation_limits_[i].second) / 2;
+      actuators_[i]->SetPosition(middle_value + actuation_value * kActuationStep);
+    }
   }
 
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr subscription_;
   std::vector<std::unique_ptr<robot::actuation::ActuationInterface>> actuators_;
+  std::vector<std::pair<float, float>> actuation_limits_;
+  
+  const int kActuationStep = 30; // TODO: Move this into local namespace.
 };
 
 int main(int argc, char * argv[]) {
-  google::InitGoogleLogging(argv[0]);
-  FLAGS_logtostderr = 1;
-
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<ActuationSubscriber>("keyboard_input"));
   rclcpp::shutdown();
---
+++ ros2/mock_actuation_publisher.cc
@@ -0,0 +1,55 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include <random>
+#include <chrono>
+
+class MockActuationPublisher : public rclcpp::Node {
+public:
+  MockActuationPublisher() : Node("mock_actuation_publisher") {
+    publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(
+      "actuation_input", 10);
+    
+    timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(100),
+      std::bind(&MockActuationPublisher::publish_mock_actuation_data, this));
+    
+    std::random_device rd;
+    random_generator_ = std::mt19937(rd());
+    distribution_ = std::uniform_real_distribution<float>(-1.0f, 1.0f);
+    
+    RCLCPP_INFO(this->get_logger(), "Mock actuation publisher started!");
+    RCLCPP_INFO(this->get_logger(), "Publishing 6-element arrays on topic: /actuation_input");
+  }
+
+private:
+  void publish_mock_actuation_data() {
+    auto message = std_msgs::msg::Float32MultiArray();
+    
+    // Generate 6 random float values between -1 and 1
+    message.data.reserve(6);
+    for (int i = 0; i < 6; ++i) {
+      float random_value = distribution_(random_generator_);
+      message.data.push_back(random_value);
+    }
+    
+    // Publish the message
+    publisher_->publish(message);
+    
+    // Log the published values
+    RCLCPP_INFO(this->get_logger(), "Published actuation array: [%.3f, %.3f, %.3f, %.3f, %.3f, %.3f]",
+                message.data[0], message.data[1], message.data[2],
+                message.data[3], message.data[4], message.data[5]);
+  }
+  
+  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::mt19937 random_generator_;
+  std::uniform_real_distribution<float> distribution_;
+};
+
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MockActuationPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
